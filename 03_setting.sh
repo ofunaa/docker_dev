e5be229e5792ff61c53789e5e0dbd5698f674143
takujifunao <takuji.funao@gmail.com>
2016-01-07 14:37:31 +0900

    wip

--- 03_setting.sh
+++ 03_setting.sh
@@ -1,4 +1,2 @@
-git clone https://github.com/takujifunao/docker_dev.git
-cd docker_dev
 docker build --build-arg USER_NAME=$0 -t rails_dev:dev .
 docker run -v $1 -d -P --name rails_go rails_dev:dev
